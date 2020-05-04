8fb4d21e6d561ed29dbfb81d8e6fbe70b88fe416
josemar <josemarsilvatxgyn@gmail.com>
2020-05-03 23:25:47 -0300

    c# exemplo de classes em vetor

---
+++ ExercicioVetoresPousada/ExercicioVetoresPousada/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Net.Mail;
+
+namespace ExercicioVetoresPousada {
+    class Program {
+        static void Main(string[] args) {
+
+            Quarto[] q; // iniciando vetor com '0' posicoes
+
+            Console.WriteLine("informe o numero de estudantes:");
+            int e = int.Parse(Console.ReadLine());
+
+            q = new Quarto[10];
+
+           for (int i = 0; i < e; i++) {
+               Console.WriteLine("Informe o nome do estudante:");
+               string nome = Console.ReadLine();
+
+               Console.WriteLine("Information o endereco de e-mail:");
+               string mail = Console.ReadLine();
+
+               Console.WriteLine("Qual numero do quarto escolhido?");
+               int quarto = int.Parse(Console.ReadLine());
+
+                q[quarto] = new Quarto { Nome = nome, Email = mail, QuartoEsc = quarto };
+            }
+
+            for (int i = 0; i < e; i++) {
+
+                if (q[i] != null) {
+                    Console.WriteLine(q[i]);
+            }
+            }
+        }
+            
+    }
+}
+
+
+    
---
+++ ExercicioVetoresPousada/ExercicioVetoresPousada/Quarto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioVetoresPousada {
+    class Quarto {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public int QuartoEsc { get; set; }
+
+        public Quarto() {
+
+        }
+
+        public Quarto(string nome, string email, int quarto) {
+
+        }
+
+        public override string ToString() {
+            return "Estudante: " + Nome + "\nEmail: " + Email + "\nNumero do quarto: " + QuartoEsc;
+        }
+    }
+}
